ecb3aed0ee5a32760dbf0b22816bad09a10df99e
Abhisekh09 <abhishek.thapa.s21@icp.edu.np>
2025-04-21 13:43:39 +0545

    feat: books data add

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className='h-screen border-r border-gray-600 min-w-48 px-6 py-4'>
-        <ul className='flex flex-col gap-4 font-bold'>
-            <li className='w-full'>
-                <NavLink to='/' clas><i class='bx bx-user'></i> User Page</NavLink>
-            </li>
-            <li>
-                <NavLink to='/accounting'><i class='bx bxs-bank'></i> Accouting</NavLink>
-            </li>
-            <li>
-                <NavLink to='/library'><i class='bx bx-book-open' ></i>  Library</NavLink>
-            </li>
-        </ul>
+    <nav className="h-screen border-r border-gray-600 min-w-48 px-6 py-4">
+      <ul className="flex flex-col gap-4 font-bold">
+        <li className="w-full">
+          <NavLink to="/">
+            <i className="bx bx-user"></i> User Page
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/accounting">
+            <i className="bx bxs-bank"></i> Accouting
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/library">
+            <i className="bx bx-book-open"></i> Library
+          </NavLink>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
